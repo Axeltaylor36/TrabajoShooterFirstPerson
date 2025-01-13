49be111c623ebac1a22e0ffd7774f2ff737ec0e8
Axeltaylor36 <alejandrosastre36@gmail.com>
2025-01-13 13:07:26 +0100

    La metralleta ya fucniona

--- Assets/Scripts/Weapon/WeaponSwitch.cs
+++ Assets/Scripts/Weapon/WeaponSwitch.cs
@@ -4,77 +4,49 @@ using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
-
     public GameObject[] weapons;
-
     public int selectedWeapon = 0;
-
-
-
     void Start()
     {
         SelectWeapon();
     }
-
-
     void Update()
     {
-
-        int previousWeapon = selectedWeapon;
-        
+        int previousWeapon = selectedWeapon; 
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-
             if(selectedWeapon >= weapons.Length-1)
             {
-
                 selectedWeapon = 0;
-
             }
             else 
             {
-            
                 selectedWeapon++;
-
             }
-
         }
-
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-
+        { 
             if (selectedWeapon <=0)
             {
-
                 selectedWeapon = weapons.Length-1;
-
             }
             else
             {
-
                 selectedWeapon--;
-
             }
-
         }
-
         if (previousWeapon != selectedWeapon) 
         {
             SelectWeapon();
-        
         }
-
     }
-
     void SelectWeapon()
     {
         int i = 0;
-
         foreach (Transform weapon in transform) 
         {
             if (weapon.gameObject.layer == LayerMask.NameToLayer("Weapon"))
             {
-
                 if(i == selectedWeapon)
                 {
                     weapon.gameObject.SetActive(true);
@@ -83,13 +55,8 @@ public class WeaponSwitch : MonoBehaviour
                 {
                     weapon.gameObject.SetActive(false);
                 }
-
                 i++;
             }
-            
-        
         }
-
     }
-
 }
